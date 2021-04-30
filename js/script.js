39f5f1add92f5038764484f49e930659440f8ebc
Jose Pereda <joselpereda@hotmail.com>
2021-04-30 02:59:29 -0400

    completed req 13

--- js/script.js
+++ js/script.js
@@ -21,6 +21,26 @@ $(function() {
     $('aside h2').css({"background-color": "#DFE3E6"});
 
 // MODIFY THE NAVIGATION MENU
-// 
+// 8. Programmatically add the class “navigation” to each of the 5 navigation menu items
+    $('nav ul li a').addClass('navigation');
+
+// 9. Select the navigation classes and apply a CSS solid, #929CA4, 1px border to them
+    $('a.navigation').css({"color":"solid #929CA4","display":"inline-block", "border":"1px solid black"});
+
+// 10. Select the navigation classes and apply a color of #CC1C0D to them
+    $('a.navigation').css({"color":"#CC1C0D"});
+
+// 11. Select the navigation classes and apply a padding of 3px to the top and bottom and 20px to the left side
+    $('a.navigation').css({"padding-top": "3px", "padding-bottom": "3px", "padding-left":"20px"});
+
+// 12. Select the navigation classes and apply a background color of #DFE3E6
+    $('a.navigation').css({"background-color": "#DFE3E6"});
+
+// MAIN CONTENT
+// 13. Programmatically remove the vprospect, vconvert, and vretain classes from the <p> tags in the main <section> tag
+    $('section p').removeClass('vprospect vconvert vretain');
+
+// 14.Programmatically add the text “Chevy Dealers Association” after the word “President” in the client testimonials section. Make sure to programmatically add a line break after the word “President” so that the new text you’re adding is placed directly underneath
+
 
 });
